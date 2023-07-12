8cb6d3735c7231c7f2a468bb4e001bbbe65af14e
t_saki <t_saki@serenegiant.com>
2023-07-13 07:59:34 +0900

    imgui_impl_glfw.hのインクルード位置を変更

--- src/internal.h
+++ src/internal.h
@@ -9,14 +9,15 @@
 #define GLFW_INCLUDE_ES3
 #if defined(EYEAPP_ENABLE_GLFW)
     #include <GLFW/glfw3.h>
+    #include "imgui_impl_glfw.h"
 #else
     #include <wayland-client.h>
     #include <wayland-cursor.h>
     #include <wayland-egl.h>
+    // FIXME waylandを自前でアクセスするimguiのバックエンドが必要
 #endif
 
 #include "imgui.h"
-#include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"		// フラグメントシェーダー等を使う場合はこっち
 // #include "imgui_impl_opengl2.h"	// これは昔の固定パイプラインを使うときのバックエンド
 
